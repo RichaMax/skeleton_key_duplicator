dc34aa7612baefea33d595a9bbd67edbc4b75904
Simon Adi <viknesh.adi@gmail.com>
2022-04-03 23:36:00 +0200

    cleaned up navigation

--- src/keys.py
+++ src/keys.py
@@ -2,36 +2,36 @@ from pynput.keyboard import Key, Controller, Listener
 from typing import Union
 from time import sleep
 
+ESCAPE_MENU_NAVIGATION = 0.1
+SOCIAL_MENU_NAVIGATION = 0.2
+EXIT_POPUP_NAVIGATION = 0.2
+
+ESCAPE_MENU_OPEN = 0.2
+SOCIAL_MENU_OPEN = 0.3
+EXIT_POPUP_OPEN = 0.2
+
 clavier = Controller()
 
-def press_key(key: Union[Key, str]):
-    clavier.press(key)
-    clavier.release(key)
+def press_key(key: Union[Key, str], times: int = 1, pause: float = 0.):
+    for _ in range(times):
+        clavier.press(key)
+        clavier.release(key)
+        sleep(pause)
 
 def continue_game():
-    press_key(Key.enter)
-    press_key(Key.enter)
+    press_key(Key.enter, times=2)
     
 def exit_to_main_menu():
-    press_key(Key.esc)
-    for _ in range(4):
-        sleep(0.2)
-        press_key(Key.down)
-    press_key(Key.enter)
-    for _ in range(2):
-        sleep(0.2)
-        press_key(Key.left)
+    press_key(Key.esc, pause=ESCAPE_MENU_OPEN)
+    press_key(Key.down, times=4, wait=ESCAPE_MENU_NAVIGATION)
+    press_key(Key.enter, wait=EXIT_POPUP_OPEN)
+    press_key(Key.left, times=2, wait=EXIT_POPUP_NAVIGATION)
     press_key(Key.enter)
     
 def open_mail():
-    press_key(Key.esc)
-    sleep(0.2)
-    press_key(Key.down)
-    sleep(0.2)
-    press_key(Key.enter)
-    for _ in range(3):
-        sleep(0.3)
-        press_key('c')
-    sleep(0.5)
+    press_key(Key.esc, wait=ESCAPE_MENU_OPEN)
+    press_key(Key.down, wait=ESCAPE_MENU_NAVIGATION)
+    press_key(Key.enter, wait=SOCIAL_MENU_OPEN)
+    press_key('c', times=3, wait=SOCIAL_MENU_NAVIGATION)
     press_key(Key.enter)
     sleep(0.2)
